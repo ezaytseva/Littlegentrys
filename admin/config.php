b4aed85e37a732f4bebfb3523a0e79b0225e0e4d
ezaytseva <ekaterina190689@yandex.ru>
2016-05-30 18:43:08 +0500

    cms без изменений

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Littlegentrys/admin/');
+define('HTTP_CATALOG', 'http://localhost/Littlegentrys/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Littlegentrys/admin/');
+define('HTTPS_CATALOG', 'http://localhost/Littlegentrys/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:\home\localhost\www\Littlegentrys/admin/');
+define('DIR_SYSTEM', 'Z:\home\localhost\www\Littlegentrys/system/');
+define('DIR_DATABASE', 'Z:\home\localhost\www\Littlegentrys/system/database/');
+define('DIR_LANGUAGE', 'Z:\home\localhost\www\Littlegentrys/admin/language/');
+define('DIR_TEMPLATE', 'Z:\home\localhost\www\Littlegentrys/admin/view/template/');
+define('DIR_CONFIG', 'Z:\home\localhost\www\Littlegentrys/system/config/');
+define('DIR_IMAGE', 'Z:\home\localhost\www\Littlegentrys/image/');
+define('DIR_CACHE', 'Z:\home\localhost\www\Littlegentrys/system/cache/');
+define('DIR_DOWNLOAD', 'Z:\home\localhost\www\Littlegentrys/download/');
+define('DIR_LOGS', 'Z:\home\localhost\www\Littlegentrys/system/logs/');
+define('DIR_CATALOG', 'Z:\home\localhost\www\Littlegentrys/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'little');
+define('DB_PASSWORD', '12345');
+define('DB_DATABASE', 'little');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Littlegentrys/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Littlegentrys/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:\home\localhost\www\Littlegentrys/catalog/');
+define('DIR_SYSTEM', 'Z:\home\localhost\www\Littlegentrys/system/');
+define('DIR_DATABASE', 'Z:\home\localhost\www\Littlegentrys/system/database/');
+define('DIR_LANGUAGE', 'Z:\home\localhost\www\Littlegentrys/catalog/language/');
+define('DIR_TEMPLATE', 'Z:\home\localhost\www\Littlegentrys/catalog/view/theme/');
+define('DIR_CONFIG', 'Z:\home\localhost\www\Littlegentrys/system/config/');
+define('DIR_IMAGE', 'Z:\home\localhost\www\Littlegentrys/image/');
+define('DIR_CACHE', 'Z:\home\localhost\www\Littlegentrys/system/cache/');
+define('DIR_DOWNLOAD', 'Z:\home\localhost\www\Littlegentrys/download/');
+define('DIR_LOGS', 'Z:\home\localhost\www\Littlegentrys/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'little');
+define('DB_PASSWORD', '12345');
+define('DB_DATABASE', 'little');
+define('DB_PREFIX', 'oc_');
+?>
